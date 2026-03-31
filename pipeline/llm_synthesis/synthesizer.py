28e86bc40182e0058985b1522d7a8b6072df42b5
Winkz0 <89746234+Winkz0@users.noreply.github.com>
2026-03-31 07:43:23 -0500

    fixed synthesizer tokens

--- pipeline/llm_synthesis/synthesizer.py
+++ pipeline/llm_synthesis/synthesizer.py
@@ -78,7 +78,7 @@ def synthesize(
 
         message = client.messages.create(
             model=MODEL,
-            max_tokens=4096,
+            max_tokens=8192,
             messages=[{"role": "user", "content": prompt}]
         )
 
